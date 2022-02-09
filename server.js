7eb74f3917e016a0f18a10561fad0997c924dcfd
Mert Mısırlıoğlu <m.misirlioglu@azerion.com>
2022-02-09 12:00:39 +0300

    express added

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-var path = require('path');
+const path = require('path');
+const express = require('express');
 
-var express = require('express');
+const app = express();
+const DIST_DIR = path.join(__dirname, '/dist');
+const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
+app.use(express.static(DIST_DIR));
+app.get('*', (req, res) => {
+    res.sendFile(HTML_FILE);
+});
 
-var app = express();
+const PORT = process.env.PORT || 8080;
 
-
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.set('port', process.env.PORT || 8080);
-
-
-var server = app.listen(app.get('port'), function() {
-
-    console.log('listening on port ', server.address().port);
+app.listen(PORT, () => {
 
 });
